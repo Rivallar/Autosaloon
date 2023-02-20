d681b281eb12d4a95dd00cfa3cfa748843175ac1
sipp <fakemail@fake.com>
2023-02-20 17:09:59 +0300

    added redis to the project

--- autosaloon/settings.py
+++ autosaloon/settings.py
@@ -86,6 +86,12 @@ DATABASES = {
         'PORT': os.getenv('DB_PORT'),
         }
     }
+    
+# REDIS
+
+REDIS_HOST = os.getenv('REDIS_HOST') 
+REDIS_PORT = os.getenv('REDIS_PORT') 
+REDIS_DB = os.getenv('REDIS_DB') 
 
 
 # Password validation
